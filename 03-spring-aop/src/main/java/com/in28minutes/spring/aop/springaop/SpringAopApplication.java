76d6e94947861db5013579b07cd8577e5765b7c1
Signviaggl <59786827+Signviaggl@users.noreply.github.com>
2024-04-12 21:49:55 +0530

    Update SpringAopApplication.java

--- 03-spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
+++ 03-spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
@@ -13,7 +13,7 @@ import com.in28minutes.spring.aop.springaop.business.Business2;
 //testing waters with the bitbucket to myBranch
 //Editing directly into the repository (GitHUB)
 //this is now direct update into the IDE
-//Another direct update in the GITHUB
+//Another direct update in the GITHUB - this is modified again
 
 @SpringBootApplication
 public class SpringAopApplication implements CommandLineRunner{
