551d246495990cf780afbbe7fbae4c1ca8bea03c
myName <myName@myEmail.com>
2024-04-12 20:58:14 +0530

    this is testing waters comment

--- 03-spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
+++ 03-spring-aop/src/main/java/com/in28minutes/spring/aop/springaop/SpringAopApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.in28minutes.spring.aop.springaop.business.Business1;
 import com.in28minutes.spring.aop.springaop.business.Business2;
 
+//testing waters with the bitbucket
 @SpringBootApplication
 public class SpringAopApplication implements CommandLineRunner{
 	
